5cec9cdb10505e1d4197b6149211201fe1bf1aa1
jiteman <jiteman.ru@ya.ru>
2022-05-27 14:52:56 +0300

    configuration has been updated

--- prepare-system-environment.sh
+++ prepare-system-environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install libxi-dev -y
+sudo apt install libxi-dev libxrandr-dev libx11-dev libxcursor-dev -y
 sudo apt install libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev -y
 sudo apt install libudev-dev -y
 
