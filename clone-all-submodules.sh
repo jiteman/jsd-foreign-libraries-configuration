aff4b0647bbbe1395d50f360e96c4554ee46b08b
jiteman <jiteman.ru@ya.ru>
2022-05-27 11:39:54 +0300

    media submodules have been added

---
+++ clone-all-submodules.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git submodule add https://github.com/dcnieho/FreeGLUT.git 0000-FreeGLUT
+git submodule add https://github.com/madler/zlib.git 0001-zlib
+git submodule add https://github.com/glennrp/libpng.git 0002-png
+git submodule add https://github.com/personal-mirrors/freetype2.git 0003-freetype2
+git submodule add https://github.com/Perlmint/glew-cmake.git 0004-glew-cmake
+git submodule add https://github.com/SDL-mirror/SDL.git 0005-SDL
+git submodule add https://github.com/kcat/openal-soft.git 0006-openal-soft
+git submodule add https://github.com/vancegroup/freealut.git 0007-freealut
+git submodule add https://github.com/xiph/ogg.git 0008-ogg
+git submodule add https://github.com/xiph/vorbis.git 0009-vorbis
+git submodule add https://github.com/xiph/flac.git 0010-flac
+git submodule add https://github.com/SFML/SFML.git 0011-SFML
